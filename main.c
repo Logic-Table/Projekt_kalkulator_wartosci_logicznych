75656d8b2d36509c34bb89fd303318fcdfe65be3
Kamil Stecyk <kamils987@interia.eu>
2020-10-16 21:26:38 +0200

    gittt

---
+++ main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  gitttt
+//
+//  Created by Kamil Stecyk on 16/10/2020.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
