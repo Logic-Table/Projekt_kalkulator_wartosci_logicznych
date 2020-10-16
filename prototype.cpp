fcef7f75f42c94ec2d8b9479c42161e98fabc2e9
Antoni Lasoń <72211818+AntoniLason@users.noreply.github.com>
2020-10-16 18:04:27 +0200

    testy
    teksy stringów

--- prototype.cpp
+++ prototype.cpp
@@ -1,9 +1,12 @@
-//prototype code 2020.10.15.17:46 Antoni Lasoñ
+//prototype code 2020.10.15.17:46 Antoni LasoÃ±
 #include <iostream>
 
 using namespace std;
 
 main()
 {
+    string a = "1234 4321";
+    int b= a[3];
+    cout<<a<<endl<<b;
     return 0;
 }
