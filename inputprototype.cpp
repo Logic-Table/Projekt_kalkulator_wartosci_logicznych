e02963456260a00c3068b0a7eece4f2e7c6b6521
Antoni Lasoń <72211818+AntoniLason@users.noreply.github.com>
2020-10-17 12:17:17 +0200

    Prototyp wejścia
    Dokładnie to co widać. Autor: Antoni Lasoń

--- inputprototype.cpp
+++ inputprototype.cpp
@@ -1,1 +1,21 @@
+//Antoni Lasoñ 2020.10.17 12:00
+#include <iostream>
+#include <string>
 
+using namespace std;
+
+main()
+{
+    string wyr;
+    getline (cin, wyr);
+    int a = wyr[2];
+    cout<<a<<endl;
+}
+
+/*
+NOTATKI
+**************************
+KODY ASCII
+**************************
+    spacja = 32
+*/
