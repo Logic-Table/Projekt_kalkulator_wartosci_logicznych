bceab621371b4b485db772182059879c4ae52a29
Kubi5 <Kubi5@users.noreply.github.com>
2020-10-20 16:59:01 +0200

    równoważność

---
+++ main.cpp
@@ -0,0 +1,20 @@
+string równowa¿noœæ(char a, char b)
+{
+    if(a=='1' && b=='1')
+    {
+        return "1";
+    }
+    if(a=='0' && b=='1')
+    {
+        return "0";
+    }
+    if(a=='1' && b=='0')
+    {
+        return "0";
+    }
+    if(a=='0' && b=='0')
+    {
+        return "1";
+    }
+    return "0";
+}
